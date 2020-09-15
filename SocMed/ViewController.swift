5ce6d2b4809b49271b440f5fd6729072edc5ca99
ismail <ismail.x@aol.com>
2020-09-15 19:06:27 +0700

    Initial Commit

---
+++ SocMed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocMed
+//
+//  Created by Ismail . on 15/09/20.
+//  Copyright © 2020 Ismail . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
